a69e571b78de1790c0eabc1f8017174c0aac6b31
Daniel Arcé <daniel.arce@gmail.com>
2024-03-29 17:51:28 -0400

    fix /docs deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
     distDir: 'docs',
     trailingSlash: true,
+    assetPrefix: isProd ? '/darce.github.io/' : '',
+    env: {
+        isProd: isProd.toString(),
+    },
     exportPathMap: async (defaultPathMap, { dev, dir, outDir, distDir, buildId }) => {
         return {
             '/': { page: '/' },
