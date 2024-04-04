6d30e9c8262d4ba5c82f9c71fd81f650cbc2d9e6
Daniel Arcé <daniel.arce@gmail.com>
2024-04-03 23:53:04 -0400

    config mdx

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/
+})
+
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
     basePath: isProd ? '/darce.github.io' : '',
     reactStrictMode: true,
     output: 'export'
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
