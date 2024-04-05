54edd89d21e967b610b8352aaa71fefe86ec5c2d
Daniel Arcé <daniel.arce@gmail.com>
2024-04-04 22:01:20 -0400

    start css styling

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,14 +7,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <div className="min-h-screen flex flex-col">
-            <main className="flex-grow container mx-auto px-4 py-8">
-                {children}
-            </main>
-            <footer>
-                {/** Footer content */}
-            </footer>
-        </div>
+        <main className="container">
+            {children}
+        </main>
     )
 }
 
