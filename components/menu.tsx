6210c92240d1a78c16195eb8c74f3d948dadabd0
Daniel Arcé <daniel.arce@gmail.com>
2024-04-08 22:44:54 -0400

    update grid & menu list style

--- components/menu.tsx
+++ components/menu.tsx
@@ -10,13 +10,14 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ projects, onSelectProject, className }) => {
     return (
         <ol className="menu">
-            {projects.map((project) => (
-                <li key={project.slug} onClick={() => onSelectProject(project)}>
-                    <h2>{project.frontMatter.title}</h2>
-                    <p>{project.frontMatter.subtitle}</p>
+            {projects.map((project, index) => (
+                <li key={project.slug + index} onClick={() => onSelectProject(project)}>
+                    <h2 className="title">{project.frontMatter.title}</h2>
+                    <p className="subtitle">{project.frontMatter.subtitle}</p>
                 </li>
-            ))}
-        </ol>
+            ))
+            }
+        </ol >
     )
 }
 
