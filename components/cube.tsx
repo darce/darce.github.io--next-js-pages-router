05b5cb30e1eb6f3cfa36ff868b3d4cf6a7cbec5a
Daniel Arcé <daniel.arce@gmail.com>
2024-04-14 14:43:11 -0400

    factor out matrix transform operations into separate lib

--- components/cube.tsx
+++ components/cube.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react'
+import * as MatrixTransforms from '../utils/matrixTransformations'
 
 interface Vertex {
     x: number
@@ -46,11 +47,11 @@ const Cube: React.FC = () => {
 
     const transformPoints = (vertexArray: Vertex[], angle: number, scale: number, distance: number) => {
         /** First rotate and scale, then project */
-        const rotatedPointsX = rotationX(vertexArray, angle)
-        const rotatedPointsY = rotationY(rotatedPointsX, angle)
-        const rotatedPointsZ = rotationZ(rotatedPointsY, angle)
-        const scaledPoints = scaleXYZ(rotatedPointsZ, scale)
-        const projectedPoints = projectPoints(scaledPoints, distance)
+        const rotatedPointsX = MatrixTransforms.rotationX(vertexArray, angle)
+        const rotatedPointsY = MatrixTransforms.rotationY(rotatedPointsX, angle)
+        const rotatedPointsZ = MatrixTransforms.rotationZ(rotatedPointsY, angle)
+        const scaledPoints = MatrixTransforms.scaleXYZ(rotatedPointsZ, scale)
+        const projectedPoints = MatrixTransforms.projectPoints(scaledPoints, distance)
 
         return projectedPoints
     }
@@ -62,111 +63,6 @@ const Cube: React.FC = () => {
         angleRef.current = newAngle
         animationRef.current = window.requestAnimationFrame(loop)
     }, [])
-
-    const projectPoints = (vertexArray: Vertex[], distance: number): Vertex[] => {
-        const result: Vertex[] = []
-        vertexArray.forEach(vertex => {
-            const f = 1 / (distance - vertex.z)
-            const projectionMatrix: number[][] = [
-                [f, 0, 0],
-                [0, f, 0],
-                [0, 0, 1]
-            ]
-            result.push(matrixMultiplyVertex(projectionMatrix, vertex))
-        })
-        return result
-    }
-
-    const rotationX = (vertexArray: Vertex[], angle: number): Vertex[] => {
-        const rotationMatrix = [
-            [1, 0, 0],
-            [0, Math.cos(angle), -Math.sin(angle)],
-            [0, Math.sin(angle), Math.cos(angle)]
-        ]
-        return transformPointsWithMatrix(vertexArray, rotationMatrix)
-    }
-
-    const rotationY = (vertexArray: Vertex[], angle: number): Vertex[] => {
-        const rotationMatrix: number[][] = [
-            [Math.cos(angle), 0, -Math.sin(angle)],
-            [0, 1, 0],
-            [-Math.sin(angle), 0, Math.cos(angle)]
-        ]
-        return transformPointsWithMatrix(vertexArray, rotationMatrix)
-    }
-
-    const rotationZ = (vertexArray: Vertex[], angle: number): Vertex[] => {
-        const rotationMatrix: number[][] = [
-            [Math.cos(angle), -Math.sin(angle), 0],
-            [Math.sin(angle), Math.cos(angle), 0],
-            [0, 0, 1]
-        ]
-        return transformPointsWithMatrix(vertexArray, rotationMatrix)
-    }
-
-    const scaleXYZ = (vertexArray: Vertex[], scale: number): Vertex[] => {
-        const scaleMatrix: number[][] = [
-            [scale, 0, 0],
-            [0, scale, 0],
-            [0, 0, 1]
-        ]
-        return transformPointsWithMatrix(vertexArray, scaleMatrix)
-    }
-
-    const transformPointsWithMatrix = useCallback((vertexArray: Vertex[], rotationMatrix: number[][]): Vertex[] => {
-        const result: Vertex[] = []
-        vertexArray.forEach(vertex => {
-            result.push(matrixMultiplyVertex(rotationMatrix, vertex))
-        })
-        return result
-    }, [])
-
-    const vertexToMatrix = (vertexObj: Vertex): number[][] => {
-        let matrix: number[][] = [
-            [vertexObj.x],
-            [vertexObj.y],
-            [vertexObj.z]
-        ]
-        return matrix
-    }
-
-    const matrixToVertex = (matrix: number[][]): Vertex => {
-        let point = {
-            x: matrix[0][0],
-            y: matrix[1][0],
-            z: matrix.length === 3 ? matrix[2][0] : 0
-        }
-        return point
-    }
-
-    const matrixMultiplyVertex = (projectionMatrix: number[][], vertex: Vertex): Vertex => {
-        const vertexMatrix: number[][] = vertexToMatrix(vertex)
-        const projectionRows: number = projectionMatrix.length
-        const projectionColumns: number = projectionMatrix[0].length
-        const pointRows: number = vertexMatrix.length;
-        const pointColumns: number = vertexMatrix[0].length;
-        if (projectionColumns !== pointRows) {
-            throw new Error('Projection columns must match point rows')
-        }
-
-        const result: number[][] = []
-
-        /** Projection rows */
-        for (let i = 0; i < projectionRows; i++) {
-            result[i] = []
-            /** Point columns (should be fixed at index 0) */
-            for (let j = 0; j < pointColumns; j++) {
-                let sum = 0;
-                /** Projection columns = point rows */
-                for (let k = 0; k < projectionColumns; k++) {
-                    sum += projectionMatrix[i][k] * vertexMatrix[k][j]
-                }
-                result[i][j] = sum
-            }
-        }
-        /** Return as point object */
-        return matrixToVertex(result)
-    }
 
     const handleClickVertex = (event: MouseEvent) => {
         const target = event.target as HTMLElement
---
+++ utils/matrixTransformations.ts
@@ -0,0 +1,107 @@
+interface Vertex {
+    x: number
+    y: number
+    z: number
+}
+
+const transformPointsWithMatrix = (vertexArray: Vertex[], rotationMatrix: number[][]): Vertex[] => {
+    return vertexArray.map(vertex => (
+        matrixMultiplyVertex(rotationMatrix, vertex)
+    ))
+}
+
+const matrixMultiplyVertex = (projectionMatrix: number[][], vertex: Vertex): Vertex => {
+    const vertexMatrix: number[][] = vertexToMatrix(vertex)
+    const projectionRows: number = projectionMatrix.length
+    const projectionColumns: number = projectionMatrix[0].length
+    const vertexRows: number = vertexMatrix.length
+    const vertexColumns: number = vertexMatrix[0].length
+    if (projectionColumns !== vertexRows) {
+        throw new Error('Projection columns must match vertex rows')
+    }
+
+    const resultMatrix: number[][] = []
+
+    /** Projection rows */
+    for (let i = 0; i < projectionRows; i++) {
+        resultMatrix[i] = [0]
+        /** Vertex columns (should be fixed at index 0) */
+        for (let j = 0; j < vertexColumns; j++) {
+            let sum = 0
+            /** Projection columns = vertex rows */
+            for (let k = 0; k < projectionColumns; k++) {
+                sum += projectionMatrix[i][k] * vertexMatrix[k][j]
+            }
+            resultMatrix[i][j] = sum
+        }
+    }
+    /** Return as vertex */
+    return matrixToVertex(resultMatrix)
+}
+
+const vertexToMatrix = (vertex: Vertex): number[][] => {
+    const matrix: number[][] = [
+        [vertex.x],
+        [vertex.y],
+        [vertex.z]
+    ]
+    return matrix
+}
+
+const matrixToVertex = (matrix: number[][]): Vertex => {
+    const vertex = {
+        x: matrix[0][0],
+        y: matrix[1][0],
+        z: matrix.length === 3 ? matrix[2][0] : 0
+    }
+    return vertex
+}
+export const rotationX = (vertexArray: Vertex[], angle: number): Vertex[] => {
+    const rotationMatrix = [
+        [1, 0, 0],
+        [0, Math.cos(angle), -Math.sin(angle)],
+        [0, Math.sin(angle), Math.cos(angle)]
+    ]
+    return transformPointsWithMatrix(vertexArray, rotationMatrix)
+}
+
+export const rotationY = (vertexArray: Vertex[], angle: number): Vertex[] => {
+    const rotationMatrix: number[][] = [
+        [Math.cos(angle), 0, -Math.sin(angle)],
+        [0, 1, 0],
+        [-Math.sin(angle), 0, Math.cos(angle)]
+    ]
+    return transformPointsWithMatrix(vertexArray, rotationMatrix)
+}
+
+export const rotationZ = (vertexArray: Vertex[], angle: number): Vertex[] => {
+    const rotationMatrix: number[][] = [
+        [Math.cos(angle), -Math.sin(angle), 0],
+        [Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 1]
+    ]
+    return transformPointsWithMatrix(vertexArray, rotationMatrix)
+}
+
+export const scaleXYZ = (vertexArray: Vertex[], scale: number): Vertex[] => {
+    const scaleMatrix: number[][] = [
+        [scale, 0, 0],
+        [0, scale, 0],
+        [0, 0, 1]
+    ]
+    return transformPointsWithMatrix(vertexArray, scaleMatrix)
+}
+
+export const projectPoints = (vertexArray: Vertex[], distance: number): Vertex[] => {
+    const result: Vertex[] = []
+    vertexArray.forEach(vertex => {
+        const f = 1 / (distance - vertex.z)
+        const projectionMatrix: number[][] = [
+            [f, 0, 0],
+            [0, f, 0],
+            [0, 0, 1]
+        ]
+        result.push(matrixMultiplyVertex(projectionMatrix, vertex))
+    })
+    return result
+}
