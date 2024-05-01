2b4ac1795e5a8ecdde2529961ec1cd44ab81cf4e
Daniel Arcé <daniel.arce@gmail.com>
2024-04-30 21:28:06 -0400

    rework mixin to reverse selector to allow removing nested layout elements

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,11 +6,9 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
-        <main className="theme--default">
-            <section className="base layout">
-                {children}
-            </section>
-        </main>
+        <div className="base theme--default layout">
+            {children}
+        </div>
     )
 }
 
