ace2cc2f2e5f12a0bf4176b51959dcbdbde7b15c
Daniel Arcé <daniel.arce@gmail.com>
2024-05-04 16:28:54 -0400

    pass subdir to getStaticProps func & update prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,31 @@ import type { ReactElement } from 'react'
 
 import Layout from '../components/Layout'
 import type { NextPageWithLayout } from './_app'
-import { getStaticProps } from '../lib/getMdxContent'
-import { ProjectData } from '../types'
+import { getStaticProps as getMdxContentStaticProps } from '../lib/getMdxContent'
+import { MarkdownData } from '../types'
 import { useSelectedProject } from '../hooks/useSelectedProject'
 
 import Menu from '../components/Menu/Menu'
 import ProjectDetails from '../components/ProjectDetails/ProjectDetails'
 
 interface WorkProps {
-    projects: ProjectData[]
+    parsedMDX: MarkdownData[]
 }
 
-const Work: NextPageWithLayout<WorkProps> = ({ projects }) => {
+const Work: NextPageWithLayout<WorkProps> = ({ parsedMDX }) => {
     const { selectedProject, handleSelectedProject, handleCloseProject } = useSelectedProject()
+
+    if (!parsedMDX || parsedMDX.length === 0) {
+        return (
+            <>
+                no parsedMDX found
+            </>
+        )
+    }
 
     return (
         <main className="content">
-            <Menu className="menu" projects={projects} onSelectProject={handleSelectedProject} />
+            <Menu className="menu" projects={parsedMDX} onSelectProject={handleSelectedProject} />
             {selectedProject &&
                 <ProjectDetails className="projectDetails" key={selectedProject.frontMatter.index} project={selectedProject} onClose={handleCloseProject} />}
         </main>
@@ -34,6 +42,9 @@ Work.getLayout = (page: ReactElement) => {
 }
 
 /** Call getStaticProps on build */
-export { getStaticProps }
+export const getStaticProps = async () => {
+    const context = { params: { subDir: 'projects' } }
+    return getMdxContentStaticProps(context)
+}
 
 export default Work
