47c0904bd799f5b74ec9fa920fcab48a55c7003a
Daniel Arcé <daniel.arce@gmail.com>
2024-05-09 09:29:13 -0400

    sort parsedMDX array only if all members have index defined

--- lib/getMdxContent.ts
+++ lib/getMdxContent.ts
@@ -1,6 +1,6 @@
 import path from 'path'
-import { GetStaticProps } from "next"
-import { getMDXFiles, parseMarkdownFile } from "./markdownUtils"
+import { GetStaticProps } from 'next'
+import { getMDXFiles, parseMarkdownFile } from './markdownUtils'
 import { MarkdownData } from '../types'
 
 interface ContentProps {
@@ -17,17 +17,23 @@ export const getStaticProps: GetStaticProps<ContentProps> = async (context) => {
             const fileName = path.basename(filePath)
             const slug = fileName.replace('.mdx', '')
             const { frontMatter, mdxSource } = await parseMarkdownFile(filePath)
-
+            const index = typeof frontMatter.index === 'number' ? frontMatter.index : null
             /** Return object for each mdx file */
             return {
                 slug,
                 frontMatter,
                 mdxSource,
-                index: frontMatter.index
+                index
             }
         }))
 
-    parsedMDX.sort((a, b) => a.index - b.index)
+    /** Only sort if index is present in all members of the array */
+    if (parsedMDX.every(mdx => mdx.index !== null)) {
+        parsedMDX.sort((a, b) => {
+            return (a.index as number) - (b.index as number)
+        }
+        )
+    }
 
     return {
         props: {
