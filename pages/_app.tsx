3e290da3eaee1857c337ecf9dc1c67841cffe2a3
Daniel Arcé <daniel.arce@gmail.com>
2024-05-10 12:22:24 -0400

    add mobile-view class to body element on resize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import '../styles/global.scss';
+import { throttle } from '../lib/utils'
+import '../styles/global.scss'
+import styles from '../styles/breakpoints.module.scss'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     getLayout?: (page: ReactElement) => ReactNode
@@ -11,7 +14,24 @@ type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout
 }
 
-export default function PortfolioApp({ Component, pageProps }: AppPropsWithLayout) {
+const PortfolioApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+    useEffect(() => {
+        const handleResize = throttle(() => {
+            /** Cast css variable */
+            const mobileMax: number = styles.mobileMax.replace('px', '') as unknown as number
+            document.body.classList.toggle('mobile-view', window.innerWidth <= mobileMax)
+        }, 200)
+
+        handleResize()
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     const getLayout = Component.getLayout ?? ((page) => page)
     return getLayout(<Component {...pageProps} />)
 }
+
+export default PortfolioApp
