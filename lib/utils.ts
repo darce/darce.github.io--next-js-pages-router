e66b1d1793532ac36d8c3cd6b03b6a341b5b5298
Daniel Arcé <daniel.arce@gmail.com>
2024-05-10 11:59:08 -0400

    implement throttle function

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,4 +9,24 @@ export const mapRange = (
     return mappedValue
 }
 
+export const throttle = (func: (...args: any[]) => void, limit: number) => {
+    type Timeout = number | NodeJS.Timeout;
+    let lastFunc: Timeout
+    let lastRan: number
 
+    return (...args: []) => {
+        const context = this
+        if (!lastRan) {
+            func.apply(context, args)
+            lastRan = Date.now()
+        } else {
+            clearTimeout(lastFunc as number)
+            lastFunc = setTimeout(() => {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args)
+                    lastRan = Date.now()
+                }
+            }, limit - (Date.now() - lastRan))
+        }
+    }
+}
