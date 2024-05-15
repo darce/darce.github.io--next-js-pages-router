b8f620a6e6e1dafcd4867f8bd6e2b32edd0a67f3
Daniel Arcé <daniel.arce@gmail.com>
2024-05-15 15:05:32 -0400

    refactor getMdxContent for better MDX file handling. rename parsedMdxArray (camel case)

--- lib/getMdxContent.ts
+++ lib/getMdxContent.ts
@@ -1,21 +1,23 @@
 import path from 'path'
-import { GetStaticProps } from 'next'
-import { getMDXFiles, parseMarkdownFile } from './markdownUtils'
+// import { GetStaticProps } from 'next'
+import { getMdxFiles, parseMarkdownFile } from './markdownUtils'
 import { MarkdownData } from '../types'
 
-interface ContentProps {
-    parsedMDXArray: MarkdownData[]
+interface GetMdxContentArgs {
+    subDir: string
 }
 
-export const getStaticProps: GetStaticProps<ContentProps> = async (context) => {
-    const subDir = context.params?.subDir as string || ''
-    const contentDir = path.join('content', subDir)
-    const mdxFiles = getMDXFiles(contentDir)
+interface MdxContentProps {
+    parsedMdxArray: MarkdownData[]
+}
 
-    const parsedMDXArray = await Promise.all(
+export const getMdxContent = async ({ subDir }: GetMdxContentArgs): Promise<MdxContentProps> => {
+    const contentDir = path.join(process.cwd(), 'content', subDir)
+    const mdxFiles = getMdxFiles(contentDir)
+
+    const parsedMdxArray = await Promise.all(
         mdxFiles.map(async (filePath) => {
-            const fileName = path.basename(filePath)
-            const slug = fileName.replace('.mdx', '')
+            const slug = path.basename(filePath).replace('.mdx', '')
             const { frontMatter, mdxSource } = await parseMarkdownFile(filePath)
             const index = typeof frontMatter.index === 'number' ? frontMatter.index : null
             /** Return object for each mdx file */
@@ -28,16 +30,14 @@ export const getStaticProps: GetStaticProps<ContentProps> = async (context) => {
         }))
 
     /** Only sort if index is present in all members of the array */
-    if (parsedMDXArray.every(mdx => mdx.index !== null)) {
-        parsedMDXArray.sort((a, b) => {
+    if (parsedMdxArray.every(mdx => mdx.index !== null)) {
+        parsedMdxArray.sort((a, b) => {
             return (a.index as number) - (b.index as number)
         }
         )
     }
 
     return {
-        props: {
-            parsedMDXArray
-        }
+        parsedMdxArray
     }
 }
