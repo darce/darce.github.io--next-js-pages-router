bcd7d0ff51d776810177d12d0fca7a6ffbfc0ba4
Daniel Arcé <daniel.arce@gmail.com>
2024-05-21 19:13:36 -0400

    update component styles

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { MarkdownData } from '../../types'
+import { MDXRemote } from 'next-mdx-remote'
+
+interface FooterProps {
+    footerData: MarkdownData[]
+    className?: string
+}
+const Footer: React.FC<FooterProps> = ({ footerData, className }) => {
+    const footerContent = footerData[0]
+    console.log(footerContent)
+    return (
+        <footer className={`${className || ''}`}>
+            <div>
+                <MDXRemote {...footerContent.mdxSource} />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
