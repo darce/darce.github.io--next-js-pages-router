4424452705acba25c957a05697babca83a2400e2
Daniel Arcé <daniel.arce@gmail.com>
2024-07-10 21:16:53 -0400

    remove prod conditional from config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-    basePath: isProd ? '/darce.github.io' : '',
+    // basePath: isProd ? '/darce.github.io' : '',
     reactStrictMode: false,
     output: 'export',
 }
